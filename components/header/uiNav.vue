7762bfd81b705c38a158c7c51641a11ae167c6bb
Carolynn <caroxlynn@gmail.com>
2020-05-05 17:42:03 +0200

    ploup

--- components/header/uiNav.vue
+++ components/header/uiNav.vue
@@ -1,18 +1,34 @@
 <template>
   <nav>
-    <button id="menuhandle">Ouvre le menu</button>
-    <ul id="mainMenu">
-        <li><nuxt-link to="/">Accueil</nuxt-link></li>
-        <li><nuxt-link to="/formation">Formation</nuxt-link></li>
-        <li><nuxt-link to="/departement">Département</nuxt-link></li>
-        <li><nuxt-link to="/mobilite">Mobilité Internationale</nuxt-link></li>
-        <li><nuxt-link to="/actualites">Actualités</nuxt-link></li>
-        <li><nuxt-link to="/contact">Contact</nuxt-link></li>
-    </ul>
+    <button id="menuhandle" @click="openMenu()">Ouvre le menu</button>
+    <div id="mainMenu">
+      <div class="wrap">
+        <ul class="pages">
+          <li><nuxt-link to="/">Accueil</nuxt-link></li>
+          <li><nuxt-link to="/formation">Formation</nuxt-link></li>
+          <li><nuxt-link to="/departement">Département</nuxt-link></li>
+          <li><nuxt-link to="/mobilite">Mobilité Internationale</nuxt-link></li>
+          <li><nuxt-link to="/actualites">Actualités</nuxt-link></li>
+          <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+        </ul>
+
+        <ul class="reseaux">
+          <li><a class="linkedin" href="#" target="_blank" title="Suivre MMI Montbéliard sur LinkedIn (Nouvelle Fenêtre)">LinkedIn</a></li>
+          <li><a class="facebook" href="#" target="_blank" title="Suivre MMI Montbéliard sur Facebook (Nouvelle Fenêtre)">Facebook</a></li>
+          <li><a class="insta" href="#" target="_blank" title="Suivre MMI Montbéliard sur Instagram (Nouvelle Fenêtre)">Instagram</a></li>
+          <li><a class="twitter" href="#" target="_blank" title="Suivre MMI Montbéliard sur Twitter (Nouvelle Fenêtre)">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
+  methods: {
+    openMenu: function() {
+      document.getElementById('mainMenu').classList.toggle('open');
+    }
+  }
 }
 </script>
--- components/uiFooter.vue
+++ components/uiFooter.vue
@@ -4,16 +4,16 @@
         <nuxt-link to="/">MMI Montbéliard</nuxt-link>
       </div>
       <ul id="links">
-        <li><a href="" target="_blank" title="Ouvrir le site de l'IUT (Nouvelle Fenêtre)">IUT Belfort Montbéliard</a></li>
-        <li><a href="" target="_blank" title="Suivre MMI Montbéliard sur LinkedIn (Nouvelle Fenêtre)">LinkedIn</a></li>
-        <li><a href="" target="_blank" title="Suivre MMI Montbéliard sur Facebook (Nouvelle Fenêtre)">Facebook</a></li>
-        <li><a href="" target="_blank" title="Suivre MMI Montbéliard sur Instagram (Nouvelle Fenêtre)">Instagram</a></li>
-        <li><a href="" target="_blank" title="Suivre MMI Montbéliard sur Twitter (Nouvelle Fenêtre)">Twitter</a></li>
-        <li><a href="" target="_blank" title="Ouvrir le site de l'Université (Nouvelle Fenêtre)">Université de France-Comté</a></li>
+        <li><a class="iut" href="#" target="_blank" title="Ouvrir le site de l'IUT (Nouvelle Fenêtre)">IUT Belfort Montbéliard</a></li>
+        <li><a class="linkedin reseaux" href="#" target="_blank" title="Suivre MMI Montbéliard sur LinkedIn (Nouvelle Fenêtre)">LinkedIn</a></li>
+        <li><a class="facebook reseaux" href="#" target="_blank" title="Suivre MMI Montbéliard sur Facebook (Nouvelle Fenêtre)">Facebook</a></li>
+        <li><a class="insta reseaux" href="#" target="_blank" title="Suivre MMI Montbéliard sur Instagram (Nouvelle Fenêtre)">Instagram</a></li>
+        <li><a class="twitter reseaux" href="#" target="_blank" title="Suivre MMI Montbéliard sur Twitter (Nouvelle Fenêtre)">Twitter</a></li>
+        <li><a class="universitefc" href="#" target="_blank" title="Ouvrir le site de l'Université (Nouvelle Fenêtre)">Université de France-Comté</a></li>
       </ul>
       <div id="contact">
-        <p>Département métiers du multimédia et de l'internet de Montbéliard</p>
-        <p><span>4, place Tharradin - 25 200 Montbéliard</span> <span>- Tél. <a href="0381994734">03 81 99 47 34</a></span></p>
+        <p class="intitule">Département métiers du multimédia et de l'internet de Montbéliard</p>
+        <p class="adresse"><span>4, place Tharradin - 25 200 Montbéliard</span> <span>- Tél. <a href="0381994734">03 81 99 47 34</a></span></p>
         <p><nuxt-link to="/mentionslegales">Mentions légales</nuxt-link></p>
       </div>
   </footer>
