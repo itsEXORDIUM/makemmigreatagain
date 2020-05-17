8dc7ca1445afdb84db3a4056d54e83fa7be6b762
Andréa <andrea.lopes25260@gmail.com>
2020-05-17 15:35:12 +0200

    headerMentionsLeg

---
+++ components/titres/mentionsleg.vue
@@ -0,0 +1,12 @@
+<template>
+  <span>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000"><defs><style>.mentionsleg-1,.mentionsleg-2,.mentionsleg-3{isolation:isolate;}.mentionsleg-2{font-size:237.15px;}.mentionsleg-2,.mentionsleg-3{fill:#2c3532;font-family:Morganite-Bold, Morganite;font-weight:700;}.mentionsleg-3{font-size:474.3px;}</style></defs><g id="MENTIONS" class="mentionsleg-1"><text class="mentionsleg-2" transform="translate(511.11 770.46)">MENTIONS</text></g><g id="LÉGALES" class="mentionsleg-1"><text class="mentionsleg-3" transform="translate(729.28 1217.88)">LÉGALES</text></g></svg>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+  }
+}
+</script>
--- pages/MentionsLegales.vue
+++ pages/MentionsLegales.vue
@@ -1,10 +1,29 @@
 <template>
-  <main id="mentionsleg"></main>
+  <main id="mentionsleg">
+    <div class="head_page">
+      <h1>
+        <span class="show-for-sr">{{titreMentionsleg}}</span>
+        <Mentionsleg />
+      </h1>
+      <button class="ancre">
+        <span class="show-for-sr">Découvrir MMI Montbéliard</span>
+      </button>
+    </div>
+  </main>
 </template>
 
 <script>
+import axios from "axios";
+import Mentionsleg from "~/components/titres/mentionsleg";
+
 export default {
   components: {
+    Mentionsleg
+  },
+  data(){
+    return{
+      titreMentionsleg: 'Mentions légales'
+    }
   }
-}
+};
 </script>
