ccccf7d6be1496bb6cc06062827059c4dd2608e7
Andréa <andrea.lopes25260@gmail.com>
2020-05-19 22:13:27 +0200

    droits dauteur

---
+++ components/blocs/blocDroitsAuteurs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="`wrap ${name}`">
+    <div class="bloc_title wrap">
+      <h2 class="bloc_titre">
+        <span class="show-for-sr">{{titre}}</span>
+        <DroitsAuteurs />
+      </h2>
+    </div>
+    <div class="bloc">
+      <h3>{{titreloi}}</h3>
+      <p>{{texteloi}}</p>
+    </div>
+    <div class="bloc">
+      <h3>{{titredroitsauteurs}}</h3>
+      <p>{{textedroitsauteurs}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import DroitsAuteurs from "~/components/titres/droitsauteurs";
+
+export default {
+  props: {
+    name: '',
+    titre: '',
+    titreloi: '',
+    texteloi: '',
+    titredroitsauteurs: '',
+    textedroitsauteurs: ''
+  },
+  components: {
+    DroitsAuteurs
+  }
+};
+</script>
--- pages/MentionsLegales.vue
+++ pages/MentionsLegales.vue
@@ -9,21 +9,45 @@
         <span class="show-for-sr">Découvrir MMI Montbéliard</span>
       </button>
     </div>
+    <BlocDroitsAuteurs :name="namedroitsauteurs" :titre="titredroitsauteurs" :titreloi="titreloi" :texteloi="texteloi" :titredroitsauteurs="titredroitsauteurs" :textedroitsauteurs="textedroitsauteurs"/>
   </main>
 </template>
 
 <script>
 import axios from "axios";
 import Mentionsleg from "~/components/titres/mentionsleg";
+import BlocDroitsAuteurs from "~/components/blocs/blocDroitsAuteurs";
 
 export default {
   components: {
-    Mentionsleg
+    Mentionsleg, BlocDroitsAuteurs
   },
-  data(){
-    return{
-      titreMentionsleg: 'Mentions légales'
-    }
+  data() {
+    return {
+      titreMentionsleg: '',
+      namedroitsauteurs: 'blocdroitsauteurs',
+      titredroitsauteurs: '',
+      titreloi: '',
+      texteloi: '',
+      titredroitsauteurs: '',
+      textedroitsauteurs: ''
+    };
+  },
+  mounted() {
+    // Get Mentionslégales Data
+    axios
+      .get("http://51.158.125.115/wp-json/wp/v2/pages/12?_embed")
+      .then(response => {
+        // Headpage
+        this.titreMentionsleg = response.data.acf.titre_mentions_legales;
+
+        // Droits d'auteurs
+        this.titredroitsauteurs = response.data.acf.titre_droits_auteurs;
+        this.titreloi = response.data.acf.groupe_1_droits_auteurs.titre;
+        this.texteloi = response.data.acf.groupe_1_droits_auteurs.texte;
+        this.titredroitsauteurs = response.data.acf.groupe_2_droits_auteurs.titre;
+        this.textedroitsauteurs = response.data.acf.groupe_2_droits_auteurs.texte;
+      });
   }
 };
 </script>
